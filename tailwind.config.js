7fd947fa44900fb182c2b20cd14b6b69d2c7fbcb
Vincent Boily <vboily@tink.ca>
2023-11-03 15:45:49 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './snippets/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
